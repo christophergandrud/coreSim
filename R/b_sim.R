21588356295398f27b8a08c50508bbb377793c38
christophergandrud <christopher.gandrud@gmail.com>
2016-09-22 12:11:23 +0200

    allow custom mu and Sigma in b_sim

--- R/b_sim.R
+++ R/b_sim.R
@@ -2,6 +2,11 @@
 #' normal distribution
 #'
 #' @param obj a fitted model object.
+#' @param mu an optional vector giving the means of the variables. If \code{obj}
+#' is supplied then \code{mu} is ignored.
+#' @param Sigma an optional positive-definite symmetric matrix specifying the
+#' covariance matrix of the variables. If \code{obj} is supplied then
+#' \code{Sigma} is ignored.
 #' @param nsim number of simulations to draw.
 #'
 #' @return A data frame of simulated coefficients from \code{obj}.
@@ -15,15 +20,29 @@
 #' # Create fitted values
 #' prestige_sims <- b_sim(m1)
 #'
+#' # Manually supply coefficient means and covariance matrix
+#' coefs <- coef(m1)
+#' vcov_matrix <- vcov(m1)
+#'
+#' prestige_sims_manual <- b_sim(mu = coefs, Sigma = vcov_matrix)
+#'
 #' @importFrom stats coef vcov
 #' @importFrom MASS mvrnorm
 #'
 #' @export
 
-b_sim <- function(obj, nsim = 1000)
+b_sim <- function(obj, mu, Sigma, nsim = 1000)
 {
-    obj_coef <- coef(obj)
-    obj_vcov <- vcov(obj)
+    if (!missing(obj)) {
+        if (!missing(mu) || !missing(Sigma))
+            message('obj used rather than mu and/or Sigma')
+        obj_coef <- coef(obj)
+        obj_vcov <- vcov(obj)
+    }
+    else if (missing(obj)) {
+        obj_coef <- mu
+        obj_vcov <- Sigma
+    }
 
     drawn <- mvrnorm(n = nsim, mu = obj_coef, Sigma = obj_vcov)
     drawn <- data.frame(drawn)
--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -7,6 +7,13 @@ test_that('b_sim output validity', {
     m1 <- lm(prestige ~ education + type, data = Prestige)
     m1_sims <- b_sim(m1)
 
+    # Manually supplied coefficient means and covariance matrix
+    coefs <- coef(m1)
+    vcov_matrix <- vcov(m1)
+    prestige_sims_manual <- b_sim(mu = coefs, Sigma = vcov_matrix)
+
+    # Preference for obj
+
     # Survival model (no intercept term)
     library(survival)
     test1 <- list(time = c(4,3,1,1,2,2,3),
@@ -17,8 +24,10 @@ test_that('b_sim output validity', {
     m_coxph <- coxph(Surv(time, status) ~ x + strata(sex), test1)
     m_coxph <- sim_coxph <- b_sim(m_coxph)
 
-
     expect_match(names(m1_sims)[[1]], 'intercept_')
+    expect_match(names(prestige_sims_manual)[[1]], 'intercept_')
+    expect_message(b_sim(obj = m1, mu = coefs, Sigma = vcov_matrix),
+                   'obj used rather than mu and/or Sigma')
     expect_match(names(sim_coxph)[[1]], 'x')
 })
 
